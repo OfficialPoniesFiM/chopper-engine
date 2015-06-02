6a103fa993a19a260966c1423d3fb00058fb7182
Nathan Guerrero <hihacks@gmail.com>
2015-06-01 21:53:05 -0700

    basegame check

---
+++ basegame.py
@@ -0,0 +1,6 @@
+import sys
+
+if __name__ == "__main__":
+  print "Wait, wait! You don't launch the game from the basegame file! You use the designated launcher! Not using it will cause multiple problems!"
+  raw_input("Press return/enter to continue. ")
+  sys.exit()
