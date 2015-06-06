c3c5e058f672cb83edcf3c4bdcb56602fd50bfad
Nathan Guerrero <hihacks@gmail.com>
2015-06-05 19:29:00 -0700

    Update basegame.py

--- basegame.py
+++ basegame.py
@@ -1,6 +1,13 @@
 import sys
+defSplashScreen = 1
 
 if __name__ == "__main__":
   print "Wait, wait! You don't launch the game from the basegame file! You use the designated launcher! Not using it will cause multiple problems!"
   raw_input("Press return/enter to continue. ")
   sys.exit()
+
+def oneTime(): # Use this for loading, terrain generation, processing, etc)
+  print "Loading game..."
+
+def gameLoop(): # Game loop. Use this for rendering, physics, whatever is needed in real time.
+  pygame.flip() # Displays the latest frame.
